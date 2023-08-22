3c314cece391969e4b00a7f97e73329e08a1fd70
Martines Gergő <martinesgergo@gmail.com>
2023-08-22 20:22:10 +0200

    feat: Encrypt function added

--- I4P-1/Program.cs
+++ I4P-1/Program.cs
@@ -4,11 +4,36 @@ using System.Linq;
 
 class Program
 {
+    static string Encrypt(string message, string key)
+    {
+        string encryptmessage = "";
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            char messageletter = message[i];
+            char keyletter = key[i % key.Length];
+
+            int messagecode = (messageletter == ' ') ? 26 : messageletter - 'a';
+            int keycode = (keyletter == ' ') ? 26 : keyletter - 'a';
+
+            int encryptlettercode = (messagecode + keycode) % 27;
+            char encryptletter = (encryptlettercode == 26) ? ' ' : (char)(encryptlettercode + 'a');
+
+            encryptmessage += encryptletter;
+        }
+
+        return encryptmessage;
+    }
+
     static void Main(string[] args)
     {
         Console.Write("Kérem adja meg az üzenetet(Csak kisbetüt vagy/és szóközt hasznájon!): ");
         string message = (Console.ReadLine());
         Console.Write("Kérem adja meg a kulcsot (Csak kisbetüt vagy/és szóközt hasznájon!): ");
         string key = (Console.ReadLine());
+
+        string encryptmessage = Encrypt(message, key);
+        Console.WriteLine("Rejtett üzenet: " + encryptmessage);
     }
+
 }
