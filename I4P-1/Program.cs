a0703b4bb7542363c7dfbd216d66d149896e86c2
Martines Gergő <martinesgergo@gmail.com>
2023-08-22 20:19:25 +0200

    feat: Main function added and user input read

--- I4P-1/Program.cs
+++ I4P-1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Kérem adja meg az üzenetet(Csak kisbetüt vagy/és szóközt hasznájon!): ");
+        string message = (Console.ReadLine());
+        Console.Write("Kérem adja meg a kulcsot (Csak kisbetüt vagy/és szóközt hasznájon!): ");
+        string key = (Console.ReadLine());
+    }
+}
