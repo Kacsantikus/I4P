7f3a5281609c4722086145dfdeaf3d2cd30c0a72
Martines Gergő <martinesgergo@gmail.com>
2023-08-22 20:29:30 +0200

    feat: Main function and file read added

---
+++ I4P-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> wordList = GetEnglishWordList();
+    }
+
+    static List<string> GetEnglishWordList()
+    {
+        List<string> wordList = new List<string>();
+
+        string[] words = File.ReadAllLines("words.txt");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            wordList.Add(words[i]);
+        }
+
+        return wordList;
+    }
+}
